46cf69f4533ca86fe308363b848ac7c04e655b69
Diana Jordan <dianacjordan@gmail.com>
2023-02-08 14:37:30 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(`${string.toUpperCase()}`);
+}
+
+function logWhisper(string){
+    console.log(`${string.toLowerCase()}`);
+}
+
+function sayHiToHeadphonedRoommate(string){
+    let msg;
+    switch(string){
+    case  "Let's have dinner together!":
+      return  msg="I would love to!"
+
+    case  string.toLowerCase():
+     return  msg="I can't hear you!"
+
+    case  string.toUpperCase():
+      return  msg="YES INDEED!"
+
+
+}
+console.log(sayHiToHeadphonedRoommate)}
